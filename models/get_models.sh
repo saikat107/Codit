c18328a8f8a8cb69c769412aaa289d9dbf4c0657
Saikat Chakraborty <saikatc@cs.columbia.edu>
2020-07-16 04:00:01 -0400

    Update get_models.sh

--- models/get_models.sh
+++ models/get_models.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# https://drive.google.com/file/d/1CL3-6dAl5J_yJH1UOwN_Qeio1_OgB-cb/
+# https://drive.google.com/file/d/1L17JJWmcRXgFa-eJvksndSl1f2f0F1Vf/
 echo "Downloading dataset"
 FILE=codit-models.zip
 if [[ -f "$FILE" ]]; then
     echo "$FILE exists, skipping download"
 else
-    fileid="1CL3-6dAl5J_yJH1UOwN_Qeio1_OgB-cb"
+    fileid="1L17JJWmcRXgFa-eJvksndSl1f2f0F1Vf"
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${FILE}
     rm ./cookie
